612351dbe44377feda6e9ceab437bb51a7f09382
Nick Richmond <nwrichmond@turnt.tech>
2020-01-10 11:26:29 -0500

    Configure theme to accept options for Nacelle credentials (id, token)

--- gatsby-theme-nacelle/gatsby-config.js
+++ gatsby-theme-nacelle/gatsby-config.js
@@ -1,15 +1,20 @@
-module.exports = {
-  plugins: [
-    {
-      resolve: 'gatsby-source-graphql',
-      options: {
-        typeName: 'Nacelle',
-        fieldName: 'nacelle',
-        url: `https://hailfrequency.com/graphql/v1/space/${process.env.NACELLE_SPACE_ID}`,
-        headers: {
-          'X-Nacelle-Token': process.env.NACELLE_GRAPHQL_TOKEN
+module.exports = options => {
+  // Be forgiving about the ways users might try to define the options
+  const nacelle_space_id = options.nacelle_space_id || options['nacelle-space-id'] || options.nacelleSpaceID || options.nacelleSpaceId
+  const nacelle_graphql_token = options.nacelle_graphql_token || options['nacelle-graphql-token']|| options.nacelleGraphQLToken || options.nacelleGraphqlToken
+  return {
+    plugins: [
+      {
+        resolve: 'gatsby-source-graphql',
+        options: {
+          typeName: 'Nacelle',
+          fieldName: 'nacelle',
+          url: `https://hailfrequency.com/graphql/v1/space/${nacelle_space_id}`,
+          headers: {
+            'X-Nacelle-Token': nacelle_graphql_token
+          },
         },
       },
-    },
-  ]
+    ]
+  }
 }
