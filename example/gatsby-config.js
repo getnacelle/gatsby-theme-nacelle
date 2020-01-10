df44c2e8f4cae5c5d1274e660704aafaca7d6ca7
Nick Richmond <nwrichmond@turnt.tech>
2020-01-10 16:31:52 -0500

    Linting

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
       resolve: `gatsby-theme-nacelle`, 
       options: {
         nacelle_space_id: process.env.NACELLE_SPACE_ID,
-        nacelle_graphql_token: process.env.NACELLE_GRAPHQL_TOKEN,
+        nacelle_graphql_token: process.env.NACELLE_GRAPHQL_TOKEN
       } 
     },
     {
