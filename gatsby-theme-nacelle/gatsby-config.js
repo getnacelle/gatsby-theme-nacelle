d56946e065c751911306631ecb30572306c199d0
Nick Richmond <nwrichmond@turnt.tech>
2020-01-10 00:21:31 -0500

    Configure theme to connect to Hail Frequency API

--- gatsby-theme-nacelle/gatsby-config.js
+++ gatsby-theme-nacelle/gatsby-config.js
@@ -1,1 +1,15 @@
-module.exports = {}
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-source-graphql',
+      options: {
+        typeName: 'Nacelle',
+        fieldName: 'nacelle',
+        url: `https://hailfrequency.com/graphql/v1/space/${process.env.NACELLE_SPACE_ID}`,
+        headers: {
+          'X-Nacelle-Token': process.env.NACELLE_GRAPHQL_TOKEN
+        },
+      },
+    },
+  ]
+}
