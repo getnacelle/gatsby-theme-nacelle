39bb11bad7bf8d08c7ea9861a68e38b51bb82574
Nick Richmond <nwrichmond@turnt.tech>
2020-01-10 00:29:20 -0500

    Provide home page links to products & collections

--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -1,3 +1,21 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Homepage in a user's site</div>
+const IndexPage = () => (
+  <>
+    <h1>DEMO: gatsby-theme-nacelle</h1>
+    <p>Let's fetch from data from Nacelle using <code>gatsby-theme-nacelle</code> <span role="img" aria-label="rocket">🚀</span>
+    </p>
+    <nav>
+      <ul style={{ listStyleType: "none" }}>
+        <li><Link to="/shop">All Products</Link></li>
+        <li><Link to="/collections/beds">Beds</Link></li>
+        <li><Link to="/collections/blankets">Blankets</Link></li>
+        <li><Link to="/collections/sheets">Sheets</Link></li>
+        <li><Link to="/collections/consoles">Consoles</Link></li>
+      </ul>
+    </nav>
+  </>
+)
+
+export default IndexPage
