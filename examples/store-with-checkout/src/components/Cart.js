98e80c9332007a1e0505997e527cea800b2a1762
Nick Richmond <nwrichmond@turnt.tech>
2020-01-15 12:05:19 -0500

    Decompose cart component for readability

--- examples/store-with-checkout/src/components/Cart.js
+++ examples/store-with-checkout/src/components/Cart.js
@@ -27,59 +27,70 @@ const CartItem = ({ item }) => {
   )
 }
 
-const Cart = () => {
-  const isCartVisible = useSelector(state => state.cart.isCartVisible)
-  const lineItems = useSelector(state => state.cart.lineItems)
+const CartItems = ({ lineItems }) => {
   const dispatch = useDispatch()
   const clearCart = () => ({ type: 'CLEAR_CART' })
-  const toggleCart = () => ({ type: 'TOGGLE_CART' })
   const total = lineItems.reduce(
     (subtotal, el) => subtotal + (Number(el.variant.price) * Number(el.variant.qty)), 0
   )
+  return(
+    <>
+      <h2 style={{ textAlign: 'center' }}>Cart</h2>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <button type="button" onClick={() => dispatch(clearCart())}>Clear Cart</button>
+      </div>
+      <ul style={{ listStyle: 'none' }}>
+      {lineItems.map(el => 
+        <li key={el.variant.id}>
+          <CartItem item={el} />
+        </li>
+      )}
+      </ul>
+      <div style={{ textAlign: 'center' }}>
+        <h3>Total</h3>
+        <p>$ {total.toFixed(2)}</p>
+      </div>
+    </>
+  )
+}
+
+const Cart = () => {
+  const lineItems = useSelector(state => state.cart.lineItems)
+  const dispatch = useDispatch()
+  const toggleCart = () => ({ type: 'TOGGLE_CART' })
   const isCartEmpty = lineItems.length === 0
   return (
     <div style={{
       position: 'fixed',
       top: '0',
-      right: '0'
+      right: '0',
+      height: '100%',
+      width: '20em',
+      overflow: 'auto',
+      border: '1px solid blue',
+      backgroundColor: 'white',
     }}>
-      {isCartVisible &&
-        <div style={{
-          position: 'fixed',
-          top: '0',
-          right: '0',
-          height: '100%',
-          width: '20em',
-          overflow: 'auto',
-          border: '1px solid blue',
-          backgroundColor: 'white',
-        }}>
-          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <button type="button" onClick={() => dispatch(toggleCart())}>Hide Cart</button>
-          </div>
-          {!isCartEmpty && <div>
-            <h2 style={{ textAlign: 'center' }}>Cart</h2>
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-              <button type="button" onClick={() => dispatch(clearCart())}>Clear Cart</button>
-            </div>
-            <ul style={{ listStyle: 'none' }}>
-            {lineItems.map(el => 
-              <li key={el.variant.id}>
-                <CartItem item={el} />
-              </li>
-            )}
-            </ul>
-            <div style={{ textAlign: 'center' }}>
-              <h3>Total</h3>
-              <p>$ {total.toFixed(2)}</p>
-            </div>
-          </div>}
-          {isCartEmpty && <p style={{ padding: '2em' }}>Your cart is empty</p>}
-        </div>
-      }
-      {!isCartVisible && <button type="button" onClick={() => dispatch(toggleCart())}>Show Cart</button>}
+      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <button type="button" onClick={() => dispatch(toggleCart())}>Hide Cart</button>
+      </div>
+      {isCartEmpty 
+        ? <p style={{ padding: '2em' }}>Your cart is empty</p>
+        : <CartItems lineItems={lineItems} />}
     </div>
   )
 }
 
-export default Cart
+const CartMenu = () => {
+  const isCartVisible = useSelector(state => state.cart.isCartVisible)
+  const dispatch = useDispatch()
+  const toggleCart = () => ({ type: 'TOGGLE_CART' })
+  return(
+    <div style={{ position: 'fixed', top: '0', right: '0' }}>
+      {isCartVisible 
+        ? <Cart />
+        : <button type="button" onClick={() => dispatch(toggleCart())}>Show Cart</button>}
+    </div>
+  )
+}
+
+export default CartMenu
