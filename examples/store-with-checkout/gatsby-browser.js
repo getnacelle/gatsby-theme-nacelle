41805c7d85221b2eb9a25db6f254f18276f5729b
Nick Richmond <nwrichmond@turnt.tech>
2020-01-14 23:07:58 -0500

    Provide Redux, middleware, and devtools to app

---
+++ examples/store-with-checkout/gatsby-browser.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { save, load } from 'redux-localstorage-simple'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import rootReducer from './src/state/root-reducer'
+import Cart from './src/components/Cart'
+
+const store = createStore(
+  rootReducer,
+  load(),
+  composeWithDevTools(applyMiddleware(save()))
+)
+
+export const wrapRootElement = ({ element }) => (
+  <Provider store={store}>{element}<Cart /></Provider>
+)
---
+++ examples/store-with-checkout/gatsby-ssr.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import rootReducer from './src/state/root-reducer'
+import Cart from './src/components/Cart'
+
+const store = createStore(
+  rootReducer,
+  {},
+  composeWithDevTools()
+)
+
+export const wrapRootElement = ({ element }) => (
+  <Provider store={store}>{element}<Cart /></Provider>
+)
