3266f618226e2296ba54d6b3eddefbb3a7d3767b
Nick Richmond <nwrichmond@turnt.tech>
2020-01-16 01:33:49 -0500

    Prefer action type constants to action strings
    Helps prevent bugs due to typos

--- examples/store-with-checkout/src/state/actions.js
+++ examples/store-with-checkout/src/state/actions.js
@@ -1,32 +1,38 @@
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const INCREMENT_ITEM = 'INCREMENT_ITEM';
+export const DECREMENT_ITEM = 'DECREMENT_ITEM';
+export const CLEAR_CART = 'CLEAR_CART';
+export const TOGGLE_CART = 'TOGGLE_CART';
+
 export function addToCart(payload) {
   return {
-    type: 'ADD_TO_CART',
+    type: ADD_TO_CART,
     payload
   };
 }
 
 export function increment(payload) {
   return {
-    type: 'INCREMENT_ITEM',
+    type: INCREMENT_ITEM,
     payload
   };
 }
 
 export function decrement(payload) {
   return {
-    type: 'DECREMENT_ITEM',
+    type: DECREMENT_ITEM,
     payload
   };
 }
 
 export function clearCart() {
   return {
-    type: 'CLEAR_CART'
+    type: CLEAR_CART
   };
 }
 
 export function toggleCart() {
   return {
-    type: 'TOGGLE_CART'
+    type: TOGGLE_CART
   };
 }
