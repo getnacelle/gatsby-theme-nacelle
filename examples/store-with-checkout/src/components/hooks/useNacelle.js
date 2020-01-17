dd2c6eae7acef8b574fa52e54ac17359032ca299
Nick Richmond <nwrichmond@turnt.tech>
2020-01-17 12:55:36 -0500

    Reusable data fetching logic; improved error boundaries

--- examples/store-with-checkout/src/components/hooks/useNacelle.js
+++ examples/store-with-checkout/src/components/hooks/useNacelle.js
@@ -1,7 +1,14 @@
 import { useState, useEffect, useCallback, useRef } from 'react';
 import axios from 'axios';
 
-function formatCredentials({ credentials = null } = {}) {
+function parseCredentials({ credentials = null } = {}) {
+  if (!credentials) {
+    throw new Error(
+      `A credentials object containing "nacelle_space_id" and 
+        "nacelle_graphql_token" properties must be passed in 
+        order to fetch data from the Hail Frequency API.`
+    );
+  }
   const nacelleSpaceId =
     credentials.nacelle_space_id ||
     credentials['nacelle-space-id'] ||
@@ -15,41 +22,56 @@ function formatCredentials({ credentials = null } = {}) {
   return [nacelleSpaceId, nacelleGraphQLtoken];
 }
 
-export function useNacelle({ credentials, query } = {}) {
+// eslint-disable-next-line prettier/prettier
+export async function getHailFrequencyData({ credentials, query, variables } = {}) {
+  if (!query) {
+    throw new Error(
+      `A query object containing a GraphQL query string
+        must be passed in order to fetch data from the
+        Hail Frequency API.`
+    );
+  }
+  const [nacelleSpaceId, nacelleGraphQLtoken] = parseCredentials({
+    credentials
+  });
+  try {
+    const result = await axios({
+      url: 'https://hailfrequency.com/v2/graphql',
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Nacelle-Space-ID': nacelleSpaceId,
+        'X-Nacelle-Space-Token': nacelleGraphQLtoken
+      },
+      data: {
+        query,
+        variables
+      }
+    });
+    return result;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export function useNacelle({ credentials, query, variables } = {}) {
   //
   // Fetch data from the Hail Frequency API with any valid query
   //
-  if (!credentials) {
-    throw new Error(
-      `useCheckout requires a credentials object containing 
-      your nacelle_space_id and nacelle_graphql_token.`
-    );
-  }
-  if (!query) {
-    throw new Error(
-      'useNacelle requires a query object containing your GraphQL query string.'
-    );
-  }
-  const [nacelleSpaceId, nacelleGraphQLtoken] = formatCredentials({
-    credentials
-  });
   const [data, setData] = useState(null);
   useEffect(
     (() => {
       async function fetchData() {
-        const result = await axios({
-          url: 'https://hailfrequency.com/v2/graphql',
-          method: 'post',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-Nacelle-Space-ID': nacelleSpaceId,
-            'X-Nacelle-Space-Token': nacelleGraphQLtoken
-          },
-          data: {
-            query
-          }
-        });
-        setData(result.data);
+        try {
+          const result = await getHailFrequencyData({
+            credentials,
+            query,
+            variables
+          });
+          setData(result.data);
+        } catch (error) {
+          throw new Error(error);
+        }
       }
       fetchData();
     },
@@ -58,78 +80,59 @@ export function useNacelle({ credentials, query } = {}) {
   return data;
 }
 
-export function useCheckout({ credentials, lineItems, checkoutId } = {}) {
+// eslint-disable-next-line prettier/prettier
+export function useCheckout({ credentials, lineItems, checkoutId = null } = {}) {
   //
   // Fetch checkout data (url, id, etc.) from the Hail Frequency API
   //
-  if (!credentials) {
-    throw new Error(
-      `useCheckout requires a credentials object containing 
-      your nacelle_space_id and nacelle_graphql_token.`
-    );
-  }
-  if (!lineItems) {
-    throw new Error(
-      `useCheckout requires a "lineItems" object containing 
-      the "variantId" and "qty" of each item in the cart.`
-    );
-  }
   const [checkoutData, setCheckoutData] = useState(null);
   const [isSending, setIsSending] = useState(false);
   const isMounted = useRef(true);
   useEffect(
+    // Set isMounted to false when the component is unmounted
     () => () => {
       isMounted.current = false;
     },
     []
   );
-  const [nacelleSpaceId, nacelleGraphQLtoken] = formatCredentials({
-    credentials
-  });
   const cartItems = lineItems.map((item, idx) => ({
     variantId: item.variant.id,
     cartItemId: `${idx}::${item.variant.id}`,
     quantity: item.variant.qty
   }));
-  const getDataCallback = useCallback(async () => {
-    if (isSending) return;
+  const checkoutCallback = useCallback(async () => {
+    if (isSending) return; // while sending, don't send again
     setIsSending(true);
-    try {
-      const result = await axios({
-        url: 'https://hailfrequency.com/v2/graphql',
-        method: 'post',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Nacelle-Space-ID': nacelleSpaceId,
-          'X-Nacelle-Space-Token': nacelleGraphQLtoken
-        },
-        data: {
-          query: `
-          mutation sendCheckout($input: CheckoutInput) {
-            processCheckout(input: $input) {
-              id
-              completed
-              url
-              source
-            }
-          }
-          `,
-          variables: {
-            input: {
-              cartItems,
-              checkoutId
-            }
-          }
+    const query = `
+      mutation sendCheckout($input: CheckoutInput) {
+        processCheckout(input: $input) {
+          id
+          completed
+          url
+          source
         }
-      });
-      setCheckoutData(result);
-      if (isMounted.current) {
-        setIsSending(false);
       }
-      return result;
+    `;
+    const variables = {
+      input: {
+        cartItems,
+        checkoutId
+      }
+    };
+    try {
+      const checkoutResult = await getHailFrequencyData({
+        credentials,
+        query,
+        variables
+      });
+      setCheckoutData(checkoutResult);
+      if (isMounted.current) {
+        setIsSending(false); // only update if still mounted
+      }
+      return checkoutResult;
     } catch (error) {
       throw new Error(error);
     }
-  }, [cartItems, checkoutId, isSending, nacelleGraphQLtoken, nacelleSpaceId]);
-  return [checkoutData, getDataCallback];
+  }, [cartItems, checkoutId, credentials, isSending]);
+  return [checkoutData, checkoutCallback];
 }
