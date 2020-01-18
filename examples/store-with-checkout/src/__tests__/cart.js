a889cb88d8ca98d49d2e1c4d11c63f242de92315
Nick Richmond <nwrichmond@turnt.tech>
2020-01-17 19:31:26 -0500

    Test that checkout button is disabled while data is loading

--- examples/store-with-checkout/src/__tests__/cart.js
+++ examples/store-with-checkout/src/__tests__/cart.js
@@ -2,36 +2,43 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+// import { renderHook, act } from '@testing-library/react-hooks';
 import Cart from '../components/Cart';
 import rootReducer from '../state/root-reducer';
+// import { useCheckout as mockUseCheckout } from '../components/hooks/useNacelle';
 
-const payload = {
-  title: 'Some Blanket',
-  handle: 'some-blanket',
-  src:
-    'https://cdn.shopify.com/s/files/1/0094/4098/5124/products/72.png?v=1563167840',
-  variant: {
-    id: '12345',
-    title: 'Default Title',
-    price: '235.0',
-    selectedOptions: [
-      {
-        name: 'Title',
-        value: 'Default Title'
-      }
-    ],
-    featuredMedia: {
-      src:
-        'https://cdn.shopify.com/s/files/1/0094/4098/5124/products/72.png?v=1563167840'
-    },
-    qty: 3
+// jest.mock('../components/hooks/useNacelle');
+
+const lineItems = [
+  {
+    title: 'Some Blanket',
+    handle: 'some-blanket',
+    src:
+      'https://cdn.shopify.com/s/files/1/0094/4098/5124/products/72.png?v=1563167840',
+    variant: {
+      id: '12345',
+      title: 'Default Title',
+      price: '235.0',
+      selectedOptions: [
+        {
+          name: 'Title',
+          value: 'Default Title'
+        }
+      ],
+      featuredMedia: {
+        src:
+          'https://cdn.shopify.com/s/files/1/0094/4098/5124/products/72.png?v=1563167840'
+      },
+      qty: 3
+    }
   }
-};
+];
 
 const initialState = {
   cart: {
-    lineItems: [payload],
-    isCartVisible: false
+    lineItems,
+    isCartVisible: false,
+    checkoutId: null
   }
 };
 
@@ -48,6 +55,7 @@ test('the cart is revealed by clicking the "Show Cart" button', () => {
 
   fireEvent.click(showCartButton);
   expect(showCartButton).not.toBeInTheDocument();
+
   const hideCartButton = getByText('Hide Cart');
   expect(hideCartButton).toBeInTheDocument();
 });
@@ -73,6 +81,7 @@ test("a product's quantity is incremented in the cart", () => {
 
   const quantityField = getByText('Quantity', { exact: false });
   expect(quantityField).toHaveTextContent('Quantity: 3');
+
   fireEvent.click(incrementButton);
   expect(quantityField).toHaveTextContent('Quantity: 4');
 });
@@ -88,6 +97,35 @@ test("a product's quantity is decremented in the cart", () => {
 
   const quantityField = getByText('Quantity', { exact: false });
   expect(quantityField).toHaveTextContent('Quantity: 4');
+
   fireEvent.click(decrementButton);
   expect(quantityField).toHaveTextContent('Quantity: 3');
+});
+
+test('the checkout button is disabled while data is loading', async () => {
+  // mockUseCheckout.mockResolvedValueOnce([
+  //   {
+  //     data: {
+  //       data: {
+  //         processCheckout: {
+  //           url: 'https://www.getnacelle.com',
+  //           id: 12345,
+  //           completed: false
+  //         }
+  //       }
+  //     }
+  //   },
+  //   () => {},
+  //   true
+  // ]);
+  const { getByText } = render(
+    <Provider store={store}>
+      <Cart />
+    </Provider>
+  );
+  const checkoutButton = getByText('Checkout');
+  expect(checkoutButton).toBeInTheDocument();
+
+  fireEvent.click(checkoutButton);
+  expect(checkoutButton).toBeDisabled();
 });
