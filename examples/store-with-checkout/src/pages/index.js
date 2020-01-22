db8dda90bdb4807a0a0d58c8f6f9e6f0206effc8
Nick Richmond <nwrichmond@turnt.tech>
2020-01-22 12:27:35 -0500

    Formatting

--- examples/store-with-checkout/src/pages/index.js
+++ examples/store-with-checkout/src/pages/index.js
@@ -1,21 +1,35 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
 const IndexPage = () => (
   <>
     <h1>DEMO: gatsby-theme-nacelle</h1>
-    <p>Let's fetch from data from Nacelle using <code>gatsby-theme-nacelle</code> <span role="img" aria-label="rocket">🚀</span>
+    <p>
+      Let's fetch from data from Nacelle using <code>gatsby-theme-nacelle</code>{' '}
+      <span role="img" aria-label="rocket">
+        🚀
+      </span>
     </p>
     <nav>
-      <ul style={{ listStyleType: "none" }}>
-        <li><Link to="/shop">All Products</Link></li>
-        <li><Link to="/collections/beds">Beds</Link></li>
-        <li><Link to="/collections/blankets">Blankets</Link></li>
-        <li><Link to="/collections/sheets">Sheets</Link></li>
-        <li><Link to="/collections/consoles">Consoles</Link></li>
+      <ul style={{ listStyleType: 'none' }}>
+        <li>
+          <Link to="/shop">All Products</Link>
+        </li>
+        <li>
+          <Link to="/collections/beds">Beds</Link>
+        </li>
+        <li>
+          <Link to="/collections/blankets">Blankets</Link>
+        </li>
+        <li>
+          <Link to="/collections/sheets">Sheets</Link>
+        </li>
+        <li>
+          <Link to="/collections/consoles">Consoles</Link>
+        </li>
       </ul>
     </nav>
   </>
-)
+);
 
-export default IndexPage
+export default IndexPage;
