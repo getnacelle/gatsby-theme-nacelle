3f603b5092507155b405d937db96c332d8b12990
Nick Richmond <nwrichmond@turnt.tech>
2020-02-14 18:00:33 -0500

    Add account page

---
+++ examples/store-with-checkout-and-styling/src/pages/account/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from 'src/components/Layout';
+
+const Account = () => {
+  const message = "Welcome to your account"
+  return (
+    <Layout><h1>Account</h1></Layout>
+  )
+}
+
+export default Account
