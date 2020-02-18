eb16f45bafee14ee6a5078cb6be848888538ef39
Nick Richmond <nwrichmond@turnt.tech>
2020-02-18 00:53:03 -0500

    Add wrapper for Facebook scripts

---
+++ examples/store-with-checkout-and-styling/src/components/FacebookClient.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const FacebookClient = () => (
+  <Helmet>
+    <script>{`
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId            : ${process.env.GATSBY_FACEBOOK_APP_ID},
+          autoLogAppEvents : true,
+          xfbml            : true,
+          version          : 'v6.0'
+        });
+      };
+    `}</script>
+    <script
+      async
+      defer
+      crossOrigin="anonymous"
+      src={`https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v6.0&appId=${process.env.GATSBY_FACEBOOK_APP_ID}`}
+    />
+    <script async defer src="https://connect.facebook.net/en_US/sdk.js" />
+  </Helmet>
+);
+
+export default FacebookClient;
