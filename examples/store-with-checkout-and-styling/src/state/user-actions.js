58a4ab693ec53f56a8b9538fae89f3a496313740
Nick Richmond <nwrichmond@turnt.tech>
2020-02-18 00:53:44 -0500

    Add login status and logout to global store

--- examples/store-with-checkout-and-styling/src/state/user-actions.js
+++ examples/store-with-checkout-and-styling/src/state/user-actions.js
@@ -1,7 +1,9 @@
 export const SET_USER_DEVICE = 'SET_USER_DEVICE';
 export const SET_CUSTOMER = 'SET_CUSTOMER';
 export const SET_CUSTOMER_ACCESS_TOKEN = 'SET_CUSTOMER_ACCESS_TOKEN';
+export const REMOVE_CUSTOMER_ACCESS_TOKEN = 'REMOVE_CUSTOMER_ACCESS_TOKEN';
 export const SET_USER_ERRORS = 'SET_USER_ERRORS';
+export const SET_LOGIN_STATUS = 'SET_LOGIN_STATUS';
 
 export function setUserDevice(payload) {
   return {
@@ -24,9 +26,22 @@ export function setCustomerAccessToken(payload) {
   };
 }
 
+export function removeCustomerAccessToken() {
+  return {
+    type: REMOVE_CUSTOMER_ACCESS_TOKEN
+  };
+}
+
 export function setUserErrors(payload) {
   return {
     type: SET_USER_ERRORS,
+    payload
+  };
+}
+
+export function setLoginStatus(payload) {
+  return {
+    type: SET_LOGIN_STATUS,
     payload
   };
 }
--- examples/store-with-checkout-and-styling/src/state/user-reducer.js
+++ examples/store-with-checkout-and-styling/src/state/user-reducer.js
@@ -2,7 +2,9 @@ import {
   SET_USER_DEVICE,
   SET_CUSTOMER,
   SET_CUSTOMER_ACCESS_TOKEN,
-  SET_USER_ERRORS
+  REMOVE_CUSTOMER_ACCESS_TOKEN,
+  SET_USER_ERRORS,
+  SET_LOGIN_STATUS
 } from './user-actions';
 
 const initialState = {
@@ -36,11 +38,25 @@ export default function(state = initialState, action) {
         }
       };
     }
+    case REMOVE_CUSTOMER_ACCESS_TOKEN: {
+      return {
+        ...state,
+        customerAccessToken: null,
+        customer: null
+      };
+    }
     case SET_USER_ERRORS: {
       const { userErrors } = action.payload;
       return {
         ...state,
         userErrors: state.userErrors.concat(userErrors)
+      };
+    }
+    case SET_LOGIN_STATUS: {
+      const { isLoggedIn } = action.payload;
+      return {
+        ...state,
+        isLoggedIn
       };
     }
     default: {
