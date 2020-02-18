5eb3300f322ca8ea40fdca68ad3ab97a9a017a2c
Nick Richmond <nwrichmond@turnt.tech>
2020-02-17 21:45:05 -0500

    Create access token and retrieve customer data

--- examples/store-with-checkout-and-styling/src/pages/account/login.js
+++ examples/store-with-checkout-and-styling/src/pages/account/login.js
@@ -1,8 +1,19 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { navigate } from 'gatsby';
 import styled from 'styled-components';
-import Layout from 'src/components/Layout';
+import { Layout } from 'src/components';
 import axios from 'axios';
 import Multipassify from 'multipassify';
+import {
+  CUSTOMER_ACCESS_TOKEN_CREATE,
+  GET_CUSTOMER
+} from 'src/queries/account';
+import {
+  setCustomer,
+  setCustomerAccessToken,
+  setUserErrors
+} from 'src/state/user-actions';
 
 const accountClient = axios.create({
   baseURL: `https://${process.env.GATSBY_MYSHOPIFY_DOMAIN}/api/2020-01/graphql`,
@@ -10,11 +21,14 @@ const accountClient = axios.create({
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
-    'X-Shopify-Storefront-Access-Token': process.env.GATSBY_SHOPIFY_GRAPHQL_TOKEN
+    'X-Shopify-Storefront-Access-Token':
+      process.env.GATSBY_SHOPIFY_GRAPHQL_TOKEN
   }
 });
 
-const multipassify = new Multipassify(process.env.GATSBY_SHOPIFY_MULTIPASS_SECRET);
+const multipassify = new Multipassify(
+  process.env.GATSBY_SHOPIFY_MULTIPASS_SECRET
+);
 
 const Form = styled.form`
   padding: 2em 1em;
@@ -26,26 +40,80 @@ const Form = styled.form`
 
 const Login = () => {
   const message = 'Login Page';
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const handleSubmit = e => {
+  const dispatch = useDispatch();
+  const customer = useSelector(state => state.user.customer);
+  async function fetchCustomer(customerAccessToken) {
+    try {
+      const query = GET_CUSTOMER;
+      const variables = {
+        customerAccessToken: customerAccessToken.accessToken
+      };
+      const response = await accountClient.post(null, { query, variables });
+      console.log(JSON.stringify(response));
+      const { data } = response.data;
+      if (data.customer) {
+        dispatch(setCustomer(data.customer));
+      }
+      if (data.userErrors) {
+        dispatch(setUserErrors(data.userErrors));
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+  const handleSubmit = async e => {
     e.preventDefault();
-    // console.log('Submitted');
-    console.log(username)
-    console.log(password)
+    try {
+      const query = CUSTOMER_ACCESS_TOKEN_CREATE;
+      const variables = { input: { email, password } };
+      const response = await accountClient.post(null, { query, variables });
+      const {
+        customerAccessToken,
+        userErrors
+      } = response.data.data.customerAccessTokenCreate;
+      if (customerAccessToken) {
+        dispatch(setCustomerAccessToken(customerAccessToken));
+        await fetchCustomer(customerAccessToken);
+      }
+      if (userErrors.length > 0) {
+        dispatch(setUserErrors(userErrors));
+      }
+      const multipassUrl = multipassify.generateUrl(
+        { ...customer, return_to: '/account' },
+        process.env.GATSBY_MYSHOPIFY_DOMAIN
+      );
+      console.log(multipassUrl);
+      if (multipassUrl) {
+        navigate(response.multipassUrl);
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
   };
   return (
     <Layout>
       <h1>{message}</h1>
       <Form onSubmit={handleSubmit}>
         <div>
-          <label>
-            <input type="text" name="username" placeholder="Username" onChange={e => setUsername(e.target.value)} />
+          <label htmlFor="email">
+            <input
+              type="email"
+              name="email"
+              placeholder="email"
+              onChange={e => setEmail(e.target.value)}
+            />
           </label>
         </div>
         <div>
-          <label>
-            <input type="text" name="password" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
+          <label htmlFor="password">
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={e => setPassword(e.target.value)}
+            />
           </label>
         </div>
         <button type="submit">Log In</button>
