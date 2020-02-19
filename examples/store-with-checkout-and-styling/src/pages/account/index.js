1a4e6fe4cae5b239a0f8e815220e17d683212df4
Nick Richmond <nwrichmond@turnt.tech>
2020-02-19 17:37:10 -0500

    Scaffold account dashboard

--- examples/store-with-checkout-and-styling/src/pages/account/index.js
+++ examples/store-with-checkout-and-styling/src/pages/account/index.js
@@ -1,10 +1,75 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import { useSelector, useDispatch } from 'react-redux';
 import { Layout } from 'src/components';
+import { accountClient } from 'src/util';
+import FacebookClient from 'src/components/FacebookClient';
+import {
+  removeCustomerAccessToken,
+  setUserErrors
+} from 'src/state/user-actions';
+import { CUSTOMER_ACCESS_TOKEN_DELETE } from 'src/queries/account';
 
-const Account = () => (
-  <Layout>
-    <h1>Account</h1>
-  </Layout>
+const Logout = () => {
+  const dispatch = useDispatch();
+  const accessToken = useSelector(
+    state => state.user.customerAccessToken.value
+  );
+  async function clearCustomer() {
+    try {
+      const query = CUSTOMER_ACCESS_TOKEN_DELETE;
+      const variables = { customerAccessToken: accessToken };
+      const response = await accountClient.post(null, { query, variables });
+      const {
+        deletedAccessToken,
+        userErrors
+      } = response.data.data.customerAccessTokenDelete;
+      if (deletedAccessToken) {
+        dispatch(removeCustomerAccessToken());
+      }
+      if (userErrors && userErrors.length > 0) {
+        dispatch(setUserErrors(userErrors));
+      }
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+  return (
+    <button type="button" onClick={clearCustomer}>
+      Log Out
+    </button>
+  );
+};
+
+const Dashboard = ({ customer }) => {
+  const { email, displayName } = customer;
+  return (
+    <>
+      <h3>Name</h3>
+      <p>{displayName}</p>
+      <h3>Email</h3>
+      <p>{email}</p>
+      <Logout />
+    </>
+  );
+};
+
+const ToLogin = () => (
+  <p>
+    Please log in <Link to="/account/login">here</Link>
+  </p>
 );
 
-export default Account;
+const AccountPage = () => {
+  const customer = useSelector(state => state.user.customer);
+  return (
+    <Layout>
+      <FacebookClient />
+      <h1>Account</h1>
+      {customer && <Dashboard customer={customer} />}
+      {!customer && <ToLogin />}
+    </Layout>
+  );
+};
+
+export default AccountPage;
