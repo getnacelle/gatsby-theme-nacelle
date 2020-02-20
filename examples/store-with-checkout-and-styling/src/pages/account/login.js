f44bb2c5655a0ab92b487dd1fa59b22c6d03736a
Nick Richmond <nwrichmond@turnt.tech>
2020-02-20 00:33:20 -0500

    Add address fetching; Improve async behavior

--- examples/store-with-checkout-and-styling/src/pages/account/login.js
+++ examples/store-with-checkout-and-styling/src/pages/account/login.js
@@ -5,14 +5,17 @@ import { Layout, EmailLogin, FacebookLogin } from 'src/components';
 import { accountClient, multipassify } from 'src/util';
 import {
   CUSTOMER_ACCESS_TOKEN_CREATE,
+  CUSTOMER_ACCESS_TOKEN_CREATE_WITH_MULTIPASS,
   CUSTOMER_CREATE,
-  GET_CUSTOMER
+  GET_CUSTOMER,
+  GET_CUSTOMER_ADDRESSES
 } from 'src/queries/account';
 import {
   setCustomer,
   setCustomerAccessToken,
   setUserErrors,
-  setLoginMethod
+  setLoginMethod,
+  setCustomerAddresses
 } from 'src/state/user-actions';
 
 const PageLayout = styled.div`
@@ -25,10 +28,15 @@ const Login = () => {
   const [userLoginMethod, setUserLoginMethod] = useState('');
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
+  const [alertMessage, setAlertMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const [shouldSubmitOnUpdate, setshouldSubmitOnUpdate] = useState(false);
+  const [needsToRegister, setNeedsToRegister] = useState(false);
+  const [needsToLogin, setNeedsToLogin] = useState(false);
   const [customerCreateCount, setCustomerCreateCount] = useState(0);
   const dispatch = useDispatch();
   const customer = useSelector(state => state.user.customer);
+  const multipassToken = useSelector(state => state.user.multipassToken);
 
   const fetchCustomer = useCallback(
     async customerAccessToken => {
@@ -38,12 +46,14 @@ const Login = () => {
           customerAccessToken: customerAccessToken.accessToken
         };
         const response = await accountClient.post(null, { query, variables });
+        setIsLoading(false);
         const { data } = response.data;
+        const { userErrors } = data;
         if (data.customer) {
           dispatch(setCustomer(data.customer));
         }
-        if (data.userErrors && data.userErrors.length > 0) {
-          dispatch(setUserErrors(...data.userErrors));
+        if (userErrors && userErrors.length > 0) {
+          dispatch(setUserErrors({ userErrors: userErrors[0] }));
         }
       } catch (error) {
         throw new Error(error);
@@ -51,6 +61,122 @@ const Login = () => {
     },
     [dispatch]
   );
+
+  const fetchAdresses = useCallback(
+    async customerAccessToken => {
+      try {
+        const variables = {
+          customerAccessToken: customerAccessToken.accessToken
+        };
+        console.log(`Variables: ${JSON.stringify(variables)}`);
+        const query = GET_CUSTOMER_ADDRESSES;
+        const response = await accountClient.post(null, { query, variables });
+        console.log(`Response: ${JSON.stringify(response)}`);
+        const { data } = response.data;
+        if (data.customer) {
+          dispatch(
+            setCustomerAddresses({
+              customerAddresses: data.customer.addresses.edges
+            })
+          );
+        }
+        if (data.userErrors && data.userErrors.length > 0) {
+          dispatch(setUserErrors({ userErrors: data.userErrors[0] }));
+          throw new Error(JSON.stringify(data.userErrors));
+        }
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    [dispatch]
+  );
+
+  const exchangeMultipassForAccessToken = useCallback(async () => {
+    try {
+      const variables = { multipassToken };
+      const query = CUSTOMER_ACCESS_TOKEN_CREATE_WITH_MULTIPASS;
+      const response = await accountClient.post(null, { query, variables });
+      setIsLoading(false);
+      const { data, errors } = response.data;
+      if (errors && errors.length) {
+        throw new Error(JSON.stringify(errors));
+      }
+      const {
+        customerAccessToken,
+        customerUserErrors
+      } = data.customerAccessTokenCreateWithMultipass;
+      if (customerAccessToken) {
+        return customerAccessToken;
+      }
+      if (customerUserErrors && customerUserErrors.length > 0) {
+        dispatch(setUserErrors({ userErrors: customerUserErrors[0] }));
+        throw new Error(JSON.stringify(customerUserErrors));
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }, [dispatch, multipassToken]);
+
+  // const multipassLogin = useCallback(async () => {
+  //   try {
+  //     if (!email) {
+  //       throw new Error('Email is empty');
+  //     }
+  //     // const query = CUSTOMER_ACCESS_TOKEN_CREATE;
+  //     // const variables = {
+  //     //   input: { email, password }
+  //     // };
+  //     // console.log(`Submitted Varibles:\n ${JSON.stringify(variables)}`);
+  //     // const response = await accountClient.post(null, { query, variables });
+  //     // const {
+  //     //   customerAccessToken,
+  //     //   userErrors
+  //     // } = response.data.data.customerAccessTokenCreate;
+  //     const customerAccessToken = await exchangeMultipassForAccessToken();
+  //     if (customerAccessToken) {
+  //       dispatch(setCustomerAccessToken(customerAccessToken));
+  //       await fetchCustomer(customerAccessToken);
+  //     }
+  //     // if (userErrors && userErrors.length > 0) {
+  //     //   console.log('User Errors:');
+  //     //   console.log(JSON.stringify(userErrors));
+  //     //   dispatch(setUserErrors({ userErrors: userErrors[0] }));
+  //     //   if (userErrors[0].message === 'Unidentified customer') {
+  //     //     if (customerCreateCount <= 1 && userLoginMethod === 'social') {
+  //     //       setNeedsToRegister(true);
+  //     //       return;
+  //     //     }
+  //     //     return new Error('too many attempts to create a new customer.');
+  //     //   }
+  //     // }
+  //     const inBrowser = window !== 'undefined';
+  //     const { protocol, host } = inBrowser
+  //       ? window.location
+  //       : { protocol: null, host: null };
+  //     const multipassifyArgs = inBrowser
+  //       ? {
+  //           ...customer,
+  //           email,
+  //           return_to: `${protocol}//${host}/account`
+  //         }
+  //       : { ...customer, email };
+  //     const multipassUrl = multipassify.generateUrl(
+  //       multipassifyArgs,
+  //       process.env.GATSBY_MYSHOPIFY_DOMAIN
+  //     );
+  //     if (multipassUrl && inBrowser) {
+  //       window.location = multipassUrl;
+  //     }
+  //   } catch (error) {
+  //     throw new Error(error);
+  //   }
+  // }, [
+  //   customer,
+  //   dispatch,
+  //   email,
+  //   exchangeMultipassForAccessToken,
+  //   fetchCustomer
+  // ]);
 
   const multipassLogin = useCallback(async () => {
     try {
@@ -63,6 +189,7 @@ const Login = () => {
       };
       console.log(`Submitted Varibles:\n ${JSON.stringify(variables)}`);
       const response = await accountClient.post(null, { query, variables });
+      setIsLoading(false);
       const {
         customerAccessToken,
         userErrors
@@ -70,18 +197,23 @@ const Login = () => {
       if (customerAccessToken) {
         dispatch(setCustomerAccessToken(customerAccessToken));
         await fetchCustomer(customerAccessToken);
+        await fetchAdresses(customerAccessToken);
       }
       if (userErrors && userErrors.length > 0) {
+        console.log('User Errors:');
+        console.log(JSON.stringify(userErrors));
+        dispatch(setUserErrors({ userErrors: userErrors[0] }));
         if (userErrors[0].message === 'Unidentified customer') {
           if (customerCreateCount <= 1) {
-            await handleRegister();
-            return;
+            if (userLoginMethod === 'social') {
+              setNeedsToRegister(true);
+              return;
+            }
+            setAlertMessage(`No account exists for this email address. 
+              To register, select Register.`);
           }
           return new Error('too many attempts to create a new customer.');
         }
-        console.log('User Errors:');
-        console.log(JSON.stringify(userErrors));
-        dispatch(setUserErrors(userErrors));
       }
       const inBrowser = window !== 'undefined';
       const { protocol, host } = inBrowser
@@ -109,9 +241,10 @@ const Login = () => {
     customerCreateCount,
     dispatch,
     email,
+    fetchAdresses,
     fetchCustomer,
-    handleRegister,
-    password
+    password,
+    userLoginMethod
   ]);
 
   const handleRegister = useCallback(
@@ -120,25 +253,48 @@ const Login = () => {
         event.preventDefault();
       }
       try {
+        setIsLoading(true);
         const variables = { input: { firstName, lastName, email, password } };
         const query = CUSTOMER_CREATE;
+        console.log(`Registering new user:\n${JSON.stringify(variables)}`);
+        if (!email) {
+          throw new Error('No email provided during registration step');
+        }
         const response = await accountClient.post(null, { query, variables });
+        setIsLoading(false);
         const { data, errors } = response.data;
-        if (errors && errors.length) {
+        if (errors && errors.length > 0) {
           throw new Error(JSON.stringify(errors));
         }
         const { customerUserErrors } = data.customerCreate;
+        console.log(JSON.stringify(customerUserErrors));
         if (customerUserErrors && customerUserErrors.length > 0) {
-          dispatch(setUserErrors(...customerUserErrors));
+          dispatch(setUserErrors({ userErrors: customerUserErrors[0] }));
+          console.log(JSON.stringify(customerUserErrors));
+          if (customerUserErrors[0].code === 'TOO_SHORT') {
+            setAlertMessage(customerUserErrors[0].message);
+          }
+          if (customerUserErrors[0].code === 'TAKEN') {
+            setAlertMessage(
+              'There is already an account associated with this email address.'
+            );
+          }
         } else {
           setCustomerCreateCount(count => count + 1);
-          await multipassLogin();
+          setNeedsToRegister(false);
+          setNeedsToLogin(true);
         }
       } catch (error) {
-        throw new Error(error);
+        if (error.message.includes('Creating Customer Limit exceeded')) {
+          setAlertMessage(
+            'There is already an account associated with this email address.'
+          );
+        } else {
+          throw new Error(error);
+        }
       }
     },
-    [dispatch, email, firstName, lastName, multipassLogin, password]
+    [dispatch, email, firstName, lastName, password]
   );
 
   const handleSubmit = useCallback(
@@ -147,6 +303,7 @@ const Login = () => {
         event.preventDefault();
       }
       if (email) {
+        setIsLoading(true);
         await multipassLogin();
       } else {
         setshouldSubmitOnUpdate(true);
@@ -156,12 +313,25 @@ const Login = () => {
   );
 
   useEffect(() => {
-    dispatch(setLoginMethod(userLoginMethod));
+    if (needsToRegister) {
+      handleRegister();
+    }
+  }, [handleRegister, needsToRegister]);
+
+  useEffect(() => {
+    if (needsToLogin) {
+      multipassLogin();
+    }
+  }, [multipassLogin, needsToLogin]);
+
+  useEffect(() => {
+    dispatch(setLoginMethod({ userLoginMethod }));
   }, [dispatch, userLoginMethod]);
 
   useEffect(() => {
     async function loginOnUpdate() {
       if (shouldSubmitOnUpdate) {
+        setIsLoading(true);
         await multipassLogin();
       }
     }
@@ -172,14 +342,14 @@ const Login = () => {
     <Layout>
       <PageLayout>
         <h1>Login Page</h1>
+        {alertMessage && <p>{alertMessage}</p>}
         <EmailLogin
           handleSubmit={handleSubmit}
           setEmail={setEmail}
           setPassword={setPassword}
           setUserLoginMethod={setUserLoginMethod}
-          setFirstName={setFirstName}
-          setLastName={setLastName}
           handleRegister={handleRegister}
+          isLoading={isLoading}
         />
         <FacebookLogin
           handleSubmit={handleSubmit}
@@ -188,7 +358,7 @@ const Login = () => {
           setUserLoginMethod={setUserLoginMethod}
           setFirstName={setFirstName}
           setLastName={setLastName}
-          handleRegister={handleRegister}
+          isLoading={isLoading}
         />
       </PageLayout>
     </Layout>
